1702d5e3cb9544b561562e30aaa92bd53e66bd8c
German1127 <kleverk2023@gmail.com>
2024-03-02 12:39:17 -0800

    add functions: show, create, destroy, update

--- console.py
+++ console.py
@@ -31,27 +31,78 @@ class HBNBCommand(cmd.Cmd):
         "Amenity": Amenity,
         "Review": Review
     }
-    def default(self, arg):
-        """Default Metod that matches arguments."""
+    def do_create(self, arg):
+        """Method that creates a new instance of BaseModel."""
+        if not arg:
+            print("** class name missing **")
+        elif arg not in self.__classes:
+            print("** class doesn't exist **")
+        else:
+            for key, val in self.__classes.items():
+                if key == arg:
+                    instance = self.__classes[key](val)
+                    FileStorage.save(instance)
+            print(f"{instance.id}")
+            
+    def do_show(self, arg):
+        """Method that prints the string representation of an instance."""
+        if not arg:
+            print("** class name missing **")
+        else:
+            args = arg.split()
+            if args[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                key = args[0] + "." + args[1]
+                all_objects = storage.all()
+                if key in all_objects:
+                    print(all_objects[key])
+                else:
+                    print("** no instance found **")
+                    
+    def do_destroy(self, arg):
+        """Method that deletes instances based on the class name and id"""
+        if not arg:
+            print("** class name missing **")
+        else:
+            args = arg.split()
+            if args[0] not in self.__classes:
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                key = args[0] + "." + args[1]
+                all_objects = storage.all()
+                if key in all_objects:
+                    del all_objects[key]
+                    storage.save()
+                else:
+                    print("** no instance found **")
+                    
+    def do_update(self, arg):
+        arguments = arg.split()
+        if not arg:
+            print("** class name missing **")
+        elif arguments[0] not in self.__classes:
+            print("** class doesn't exist **")
+        elif len(arguments) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{arguments[0]}.{arguments[1]}"
+            if key not in storage.all().keys():
+                print("** no instance found **")
+            elif len(arguments) < 3:
+                print("** attribute name missing **")
+            elif len(arguments) < 4:
+                print("** value missing **")
+            else:
+                content = arguments[3]
 
-        argdict = {
-            "all": self.do_all,
-            "show": self.do_show,
-            "destroy": self.do_destroy,
-            "create": self.do_create,
-            "update": self.do_update,
-        }
-        match = re.search(r"\.", arg)
-        if match is not None:
-            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
-            match = re.search(r"\((.*?)\)", argl[1])
-            if match is not None:
-                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
-                if command[0] in argdict.keys():
-                    call = "{} {}".format(argl[0], command[1])
-                    return argdict[command[0]](call)
-        print("*** Unknown syntax: {}".format(arg))
-        return False
+                element = storage.all()[key]
+                element.__setattr__(arguments[2], content)
+                element.save()
 
     def do_quit(self, arg):
         """Method that quits the program."""
