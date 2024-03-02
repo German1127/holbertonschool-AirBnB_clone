1498922b5ae2c3074b1f6e3b26fce03ad12e8f87
German1127 <kleverk2023@gmail.com>
2024-03-02 12:27:07 -0800

    add and update

--- console.py
+++ console.py
@@ -3,31 +3,71 @@
 
 
 import cmd
-"""contains the shell entry point"""
+import re
+import json
+
+from shlex import split
+from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """class HBNB command """
     prompt = "(hbnb)"
-
-    def do_EOF(self, arg):
-        """Command for quit the program"""
-        print()
-        return True
+    
+    classes = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Place": Place,
+        "Amenity": Amenity,
+        "Review": Review
+    }
 
     def do_quit(self, arg):
-        """Command for quit the program"""
+        """Method that quits the program."""
         return True
 
-    def help_EOF(self):
-        print("Exit the program on EOF (ctrl-D).")
+    def do_EOF(self, arg):
+        """Method for the end of file."""
+        return True
 
     def help_quit(self):
-        print("Exit the program.")
+        """Method that manages the help for the commands.s"""
 
-    def emptyline(self):
-        """an empty line + ENTER shouldn’t execute anything"""
-        pass
+        print("Quit command to exit the program.")
+
+    def help_create(self):
+        """Method that manages the help commando for the create method."""
+        print("Create command to create a new insance.")
+
+    def help_show(self):
+        """Method that manages the help command for the show method."""
+        print("Show command prints on screen infromation about instances.")
+
+    def help_destroy(self):
+        """Method that manages the help command for the destroy method."""
+        print("Destroy command deletes instances.")
+
+    def help_all(self):
+        """Method that manages the help command for the all method."""
+        print("All command displays all of the instances.")
+
+    def help_EOF(self):
+        """Method that manages the help command for the EOF method."""
+        print("EOF command exits the program.")
+
+    def help_update(self):
+        """ Method that manages the help command for the update method. """
+        print("The Update command updates the class information.")
 
 
 if __name__ == "__main__":
