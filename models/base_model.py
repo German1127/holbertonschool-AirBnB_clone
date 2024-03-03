be7d585611834b220b10488f17ef1d5e9ad498e2
German1127 <kleverk2023@gmail.com>
2024-03-02 16:40:02 -0800

    update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,41 @@
 #!/usr/bin/python3
-"""Class base"""
+"""Class BaseModel that defines all common attributes/methods."""
 import uuid
-from datetime import datetime
 import models
+from datetime import datetime
 
 
 class BaseModel:
-    """Base class"""
+    """Class BaseModel"""
 
     def __init__(self, *args, **kwargs):
-        """Constructor method"""
-        timeformat = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
-            if len(kwargs) != 0:
-                for k, v in kwargs.items():
-                    if k == "__class__":
-                        continue
-                    if k == "created_at" or k == "updated_at":
-                        self.__dict__[k] = datetime.strptime(v, timeformat)
-                    else:
-                        self.__dict__[k] = v
+        """ re-create an instance """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """String representation"""
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        """Method that prints."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """update"""
+        """ Method that updates the public instance attribute."""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary"""
-        direc = self.__dict__.copy()
-        direc["__class__"] = type(self).__name__
-        direc["created_at"] = self.created_at.isoformat()
-        direc["updated_at"] = self.updated_at.isoformat()
-        return direc
+        """ Method that returns a dictionary containing keys/values """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
